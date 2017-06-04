65b93543dd97f10e7620ea0b76dd571ce0d2281d
Ubuntu <ubuntu@ip-172-31-11-148.us-west-2.compute.internal>
2017-06-04 13:23:53 +0000

    I'm adding madLibs

---
+++ bushra/madLibs..py
@@ -0,0 +1,16 @@
+print("Please provide the following types of words:");
+firstVal = input("Name: ");
+secondVal = input("Plural Noun: ");
+thirdVal = input("Plural Noun: ");
+fourthVal = input("Adjectives: ");
+fifthVal = input("Pick one: Mr. or Mrs.: ");
+sixthVal = input("Noun: ");
+
+print("""Let's get down to business
+ To defeat the %s
+ Did they send me %s 
+ When I asked for %s
+ You're the %s bunch
+ I ever met, but you can bet
+ Before we're through
+ %s ,I'll make a %s out of you.""" % (firstVal, secondVal, thirdVal, fourthVal, fifthVal, sixthVal));   
