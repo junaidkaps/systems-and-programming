033a5d3a1561b9f6daf16da2bd507435c650f059
Aaiysha Mistry <aaiyshamistry98@gmail.com>
2017-06-04 13:25:23 +0000

    Adding madlibs

---
+++ aaiysha/madlibs.py
@@ -0,0 +1,14 @@
+print("Please provide the following words: ")
+
+country = input("Country: ")
+adjective = input("Adjective: ")
+verb = input("Verb: ")
+noun = input("Noun: ")
+verb1 = input("Verb: ")
+pronoun = input("Pronoun: ")
+noun1 = input("Noun: ")
+country1 = input("Country: ")
+
+print("We the People of the %s, in order to form a more %s union, %s justice, insure %s Tranquility, promote the general Welfare, and %s the Blessings of Liberties to %s and our Posterity, do ordain and establish this %s for the %s." %(country,adjective, verb, noun, verb1, pronoun, noun1, country1)) 
+
+ 
